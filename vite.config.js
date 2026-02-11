c04e209ec112503a8b90bea53a16f2980d6b5ae3
Wu Xie <153907200+jy02140251@users.noreply.github.com>
2026-02-12 02:37:46 +0800

    Add Vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'Component',
+      fileName: 'component'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: { vue: 'Vue' }
+      }
+    }
+  }
+})
